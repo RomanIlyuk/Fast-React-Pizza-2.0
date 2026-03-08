635cf12252a36e9848059dd73e5d88ed47fa3535
riliu <ilyukroma228@gmail.com>
2026-03-08 12:57:38 +0200

    Added some flexbox layout

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="hidden text-sm font-semibold">Roman</div>;
+  return <div className="hidden text-sm font-semibold md:block">Roman</div>;
 }
 
 export default UserName;
